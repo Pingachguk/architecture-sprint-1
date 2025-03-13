fb9384213518a92408b6f45b20edb37a359edd43
Андрей Якимов <yakimov.a@promail366.com>
2025-03-14 01:39:08 +0300

    Выполняем 1 и 2 уровень первого задания: определяем фрейморк для конфигурации микрофронтендов и определяем структуру микрофронтендов

---
+++ frontend/microfrontend/auth/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const App = () => (
+    <div className="base">
+        <h1>hi</h1>
+    </div>
+);
+ReactDOM.render(<App/>, document.getElementById("app"));
